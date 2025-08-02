5c29d3918df431de0936a3a8e393cdcc67b22caa
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-08-02 19:23:58 +0530

    new builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.', // Root directory containing index.html
-  publicDir: './client/public', // Directory for static assets
+  publicDir: './public', // Directory for static assets
   build: {
-    outDir: 'dist', // Output directory within the client folder
+    outDir: 'dist', // Output directory
     sourcemap: true, // Enable source maps for debugging production builds
     emptyOutDir: true // Clear output directory before building
   },
